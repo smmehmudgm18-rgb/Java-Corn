a28ae99ef7262960d9f61fd808f1575f763b6d8a
Java Core <smmehmudgm18@gmail.com>
2026-09-23 11:58:24 +0600

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCX7u6tAmK-k-6Oq-S7DNhU74MyyfT38Uw",
   authDomain: "java-corn.firebaseapp.com",
@@ -17,6 +10,9 @@ const firebaseConfig = {
   measurementId: "G-TKDER5WR36"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase (Compat version)
+firebase.initializeApp(firebaseConfig);
+
+// Global instances for other files to use easily
+const auth = firebase.auth();
+const db = firebase.firestore();
